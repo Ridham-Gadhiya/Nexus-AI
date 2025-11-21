cf94b6c2b707531c23e5a7e59abb4a58eab12f6d
Ridham-Gadhiya <ridhamgadhiya16@gmail.com>
2025-11-21 22:07:32 +0530

    commit

--- backend/nexus/work/urls.py
+++ backend/nexus/work/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('create_work/', WorkCreateView.as_view(), name='create_work'),
     path('list_work/', WorkListView.as_view(), name='list_work'),
     path('detail_list_work/<int:id>/', WorkDetailView.as_view(), name='detail_list_work'),
-    path('update_work/', WorkUpdateView.as_view(), name='update_work'),
+    path('update_work/<int:id>/', WorkUpdateView.as_view(), name='update_work'),
 
 
 
