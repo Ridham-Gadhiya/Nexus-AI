f873eee27da2689d8f76b7058b8c2adb2478b159
Ridham-Gadhiya <ridhamgadhiya16@gmail.com>
2025-11-21 22:33:20 +0530

    commit

--- backend/nexus/developer/views.py
+++ backend/nexus/developer/views.py
@@ -27,3 +27,38 @@ class DeveloperListView(generics.ListAPIView):
     queryset = Developer.objects.all()
     serializer_class = DeveloperSerializer
     permission_classes = [AllowAny]
+    
+class DeveloperUpdateView(generics.UpdateAPIView):
+    queryset = Developer.objects.all()
+    serializer_class = DeveloperSerializer
+    permission_classes = [IsAdminUser]
+    lookup_field = "id"
+    
+    def update(self, request, *args, **kwargs):
+        developer = self.get_object()
+
+        serializer = self.get_serializer(developer, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({
+            'message': "Project has been Updated",
+            'developer_id': developer.id,
+            'developer_name': developer.name,
+            'developer_email': developer.email,
+            'developer.bio': developer.bio,
+            'developer.role': developer.role,
+            'developer_thumbnail': str(developer.thumbnail),
+            'developer_linkedln':str(developer.linkedln),
+            'developer_github':developer.github
+        }, status=status.HTTP_200_OK)
+
+class DeveloperDeleteView(generics.DestroyAPIView):
+    queryset = Developer.objects.all()
+    serializer_class = DeveloperSerializer
+    permission_classes = [IsAdminUser]
+    lookup_field = "id"
+    
+    def delete(self, request, *args, **kwargs):
+        developer = self.get_object()
+        developer.delete()
+        return Response({'message': "Project has been Deleted"}, status=status.HTTP_200_OK)
