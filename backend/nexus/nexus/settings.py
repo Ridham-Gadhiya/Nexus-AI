57ada03ce761ac3605e05f10546820d374a1dfb6
Ridham Gadhiya <ridhamgadhiya16@gmail.com>
2026-04-09 10:51:21 +0530

    commit

--- backend/nexus/nexus/settings.py
+++ backend/nexus/nexus/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # This loads the variables from your .env file
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # Quick-start development settings - unsuitable for production
